dee82e67ecc1c3a594e0cd7fdddb7591ed1a6d45
madni2886 <muhammad.madni@nxb.com.pk>
2022-08-23 17:06:13 +0500

    messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,18 @@ class SessionsController < ApplicationController
   end
   def create
     user=User.find_by(email: params[:session][:email].downcase)
-    if(user&&user.authenticate(params[:session][:password]))
-      session[:user_id]=user.id
-      flash[:success] = "You have successfully logged in "
-      redirect_to user_path(user)
-    else
-      flash.now[:danger]='there is something wrong with your login information'
-      render 'new'
+    respond_to do |format|
+      if user && user.authenticate(params[:session][:password])
+         session[:user_id] = user.id
+         format.html{redirect_to users_path(user), notice: "You have successfully logged in." } 
+      else
+        flash[:unprocessable_entity] = "Wrong email or password"
+         format.html{render :new, status: :unprocessable_entity } 
+      end
     end
   end
+
+ 
 
   def destroy
     session[:user_id]=nil
